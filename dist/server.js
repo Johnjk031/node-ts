b7393bbaf119fc7bb10d744251b250fb29b1f0be
Johnjk031 <john.juhlinkarlsson@gmail.com>
2021-08-25 12:08:31 +0200

    kladdplank

---
+++ dist/server.js
@@ -0,0 +1,19 @@
+/*
+let http = require('http')
+const fs = require('fs')
+const port = 1010
+var express = require('express');
+var path = require('path');
+const server = express()
+
+
+server.listen(port, function(error: any) {
+    if (error) {
+        console.log('whoops', error)
+    } else {
+        console.log('Server is running on localhost:' + port)
+    }
+})
+*/
+let array = ['alfa', 'beta', 'gamma'];
+console.log(array);
